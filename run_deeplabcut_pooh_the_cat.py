1d3300c797e3a525fd7582485933d9ca519dfc10
jonmatthis <jonmatthis@gmail.com>
2024-02-28 15:30:02 -0500

    build from pre-trained model

--- run_deeplabcut_pooh_the_cat.py
+++ run_deeplabcut_pooh_the_cat.py
@@ -1,13 +1,23 @@
-# %% import stuff 
-import deeplabcut 
+# %% import stuff
+import deeplabcut
 
+# %% create project
+response = deeplabcut.create_pretrained_project(
+    "pooh-the-cat",
+    "jsm",
+    [r"C:\Users\jonma\Sync\videos\2024-02-28_pooh_gopros\GX010034_clip1.mp4"],
+    model="full_cat",
+    working_directory=r"D:\deeplabcut-models",
+    copy_videos=True,
+    videotype=".mp4",
+    analyzevideo=True,
+    filtered=True,
+    createlabeledvideo=True,
+    trainFraction=None,
+)
 
-# %% create or load project
-deeplabcut.create_new_project('pooh-the-cat',
-                               'jsm', 
-                               [r"C:\Users\jonma\Sync\videos\2024-02-28_pooh_gopros\GX010034_clip1DLC_snapshot-700000_labeled.mp4"], 
-                               working_directory=r'D:\deeplabcut-models\pooh-the-cat', 
-                               copy_videos=True,)
-
+print("-----------------------------")
+print(f"Created project: \n\n {response}")
+print("-----------------------------")
 
 # %%

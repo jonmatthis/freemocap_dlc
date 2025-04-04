8afbe5d72fbafb88ace5da18c3a77299a25304b5
philipqueen <queen.philip@gmail.com>
2025-04-04 11:46:27 -0600

    Add optional annotation

--- scholl_lab/analyze_videos.py
+++ scholl_lab/analyze_videos.py
@@ -39,7 +39,7 @@ def merge_csvs_for_skellyclicker(csv_folder_path: str | Path, output_path: str |
     print(f"Saved skellyclicker compatible CSV to {output_path}")
 
 
-def analyze_videos(config_path: str | Path, path_to_recording_folder: str | Path | None = None):
+def analyze_videos(config_path: str | Path, path_to_recording_folder: str | Path | None = None, annotate_videos: bool = False):
     config = auxiliaryfunctions.read_config(config_path)
     if path_to_recording_folder is None:
         path_to_recording_folder = config["skellyclicker_folder_of_videos"]
@@ -51,8 +51,16 @@ def analyze_videos(config_path: str | Path, path_to_recording_folder: str | Path
         save_as_csv=True,
     )
 
-    merge_csvs_for_skellyclicker(path_to_recording_folder, Path(config["project_path"]) / f"skellyclicker_output_iteration_{config['iteration']}.csv")
+    output_folder = Path(config["project_path"]) / f"model_outputs_iteration_{config['iteration']}"
+    output_folder.mkdir(parents=True, exist_ok=True)
+    if annotate_videos:
+        print(f"Annotating videos in {path_to_recording_folder}, saving to {output_folder}")
+        deeplabcut.create_labeled_video(config=config_path, videos=str(path_to_recording_folder), videotype=".mp4", destfolder=str(path_to_recording_folder))
+    else:
+        print("Skipping video annotation")
+
+    merge_csvs_for_skellyclicker(path_to_recording_folder, output_folder / f"skellyclicker_machine_labels_iteration_{config['iteration']}.csv")
 
 if __name__ == "__main__":
     config_path = Path("/Users/philipqueen/DLCtest/sample_data_test2_user_20250403/config.yaml")
-    analyze_videos(config_path=config_path)
+    analyze_videos(config_path=config_path, annotate_videos=False)
